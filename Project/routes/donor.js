9181ed879e57b2549d1db27e93091c8a37fd3edc
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2022-01-22 16:48:26 +0300

    Add files via upload

--- Project/routes/donor.js
+++ Project/routes/donor.js
@@ -18,6 +18,25 @@ router
   });
 
 });
+//View blood test results
+router.get('/donor/result' , authentication.isDonorLoggedIn ,(req , res)=> {
+ let sql = `SELECT * FROM donor 
+            INNER JOIN donor_health 
+            ON donor.donor_info_ID = donor_health.donor_ID
+            INNER JOIN test_result
+            ON donor_health.test_result_ID = test_result.Testtube_No
+            WHERE donor_info_ID= "${req.userData.Username}"
+            ORDER BY Date_Tested DESC`;
+ 
+
+
+ db.query(sql, (error , result) =>{
+ 
+  res.render('donor/results', {user:result});
+  });
+
+  
+});
 
 //Change Password
 router.get('/donor/changepassword' , authentication.isDonorLoggedIn ,(req , res)=> {
