b29cee91dce33174e88a222aed49ac59fee28df8
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2022-01-25 13:43:14 +0300

    Add files via upload

--- Database/BloodDonation.sql
+++ Database/BloodDonation.sql
@@ -49,6 +49,34 @@ INSERT INTO `admin` VALUES (1,'Nathan Zelalem','a@gmail.com','Male','1999-02-01'
 UNLOCK TABLES;
 
 --
+-- Table structure for table `appointment`
+--
+
+DROP TABLE IF EXISTS `appointment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `appointment` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `donor_username` varchar(50) NOT NULL,
+  `dateTime_of_appointment` datetime NOT NULL,
+  `location` varchar(100) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `donor_username_idx` (`donor_username`),
+  CONSTRAINT `donor_username` FOREIGN KEY (`donor_username`) REFERENCES `donor` (`donor_info_ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `appointment`
+--
+
+LOCK TABLES `appointment` WRITE;
+/*!40000 ALTER TABLE `appointment` DISABLE KEYS */;
+INSERT INTO `appointment` VALUES (1,'NahBeh','2022-01-25 03:30:00','Stadium Blood Bank');
+/*!40000 ALTER TABLE `appointment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `donation_history`
 --
 
@@ -99,7 +127,7 @@ CREATE TABLE `donor` (
 
 LOCK TABLES `donor` WRITE;
 /*!40000 ALTER TABLE `donor` DISABLE KEYS */;
-INSERT INTO `donor` VALUES ('Lealem','Kinfe','l@gmail.com','Male','2000-01-01','LeaKin'),('Nahom','Behailu','n@gmail.com','Male','2000-01-04','NahBeh');
+INSERT INTO `donor` VALUES ('Lealem','Kinfe','l@gmail.com','Male','2000-01-01','LeaKin'),('Nahom','Behailu','n@gmail.com','Male','2000-01-04','NahBeh'),('Natnael','Tedros','nati@gmail.com','Male','2000-03-10','NatTed');
 /*!40000 ALTER TABLE `donor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -128,7 +156,7 @@ CREATE TABLE `donor_health` (
   CONSTRAINT `health_info_ID` FOREIGN KEY (`health_info_ID`) REFERENCES `health_info` (`ID`),
   CONSTRAINT `inventory_ID` FOREIGN KEY (`inventory_ID`) REFERENCES `inventory` (`BloodBagNo`),
   CONSTRAINT `test_result_ID` FOREIGN KEY (`test_result_ID`) REFERENCES `test_result` (`Testtube_No`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -137,7 +165,7 @@ CREATE TABLE `donor_health` (
 
 LOCK TABLES `donor_health` WRITE;
 /*!40000 ALTER TABLE `donor_health` DISABLE KEYS */;
-INSERT INTO `donor_health` VALUES (1,NULL,NULL,934433,NULL,'NahBeh'),(2,NULL,NULL,124562,NULL,'LeaKin'),(3,NULL,NULL,123432,NULL,'NahBeh');
+INSERT INTO `donor_health` VALUES (1,1,NULL,934433,NULL,'NahBeh'),(2,NULL,NULL,124562,NULL,'LeaKin'),(3,NULL,NULL,123432,NULL,'NahBeh'),(4,NULL,NULL,696969,NULL,'NatTed');
 /*!40000 ALTER TABLE `donor_health` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -155,7 +183,7 @@ CREATE TABLE `health_info` (
   `Blood_Pressure` varchar(100) NOT NULL,
   `Haemoglobin_Level` double NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -164,6 +192,7 @@ CREATE TABLE `health_info` (
 
 LOCK TABLES `health_info` WRITE;
 /*!40000 ALTER TABLE `health_info` DISABLE KEYS */;
+INSERT INTO `health_info` VALUES (1,'O','-','180/120',120.5);
 /*!40000 ALTER TABLE `health_info` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -175,8 +204,9 @@ DROP TABLE IF EXISTS `inventory`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `inventory` (
-  `Amount` double NOT NULL,
   `BloodBagNo` int NOT NULL,
+  `Amount` double NOT NULL,
+  `Blood_Viability` enum('Viable','Nonviable') NOT NULL,
   PRIMARY KEY (`BloodBagNo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -216,7 +246,7 @@ CREATE TABLE `personal_info` (
 
 LOCK TABLES `personal_info` WRITE;
 /*!40000 ALTER TABLE `personal_info` DISABLE KEYS */;
-INSERT INTO `personal_info` VALUES ('BisAsh','4444',969696969,'A.A','Bole','03','122'),('LeaKin','3333',911232328,'Everywhere','Bole','09','234'),('LilAle','2222',911232327,'Here','Kolfe','02','123'),('NahBeh','1111',943555555,'Adama','Rep','09','123'),('NatZel','0000',911232323,'A.A','Bole','01','new');
+INSERT INTO `personal_info` VALUES ('BisAsh','4444',969696969,'A.A','Bole','03','122'),('LeaKin','3333',911232328,'Everywhere','Bole','09','234'),('LilAle','2222',911232327,'Here','Kolfe','02','123'),('NahBeh','1111',943555555,'Adama','Rep','09','123'),('NatTed','0069',934341281,'Addis Ababa','Kolfe Keranyo','09','123'),('NatZel','0000',911232323,'A.A','Bole','01','new');
 /*!40000 ALTER TABLE `personal_info` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -235,8 +265,12 @@ CREATE TABLE `test_result` (
   `Chlamydia` enum('Positive','Negative') NOT NULL,
   `Syphilis` enum('Positive','Negative') NOT NULL,
   `Date_Tested` date NOT NULL,
+  `Counseling` enum('Received','Not Received') DEFAULT NULL,
+  `Approved_By` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`Testtube_No`),
-  UNIQUE KEY `Testtube_No_UNIQUE` (`Testtube_No`)
+  UNIQUE KEY `Testtube_No_UNIQUE` (`Testtube_No`),
+  KEY `Approved_By_idx` (`Approved_By`),
+  CONSTRAINT `Approved_By` FOREIGN KEY (`Approved_By`) REFERENCES `admin` (`admin_info_ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=934434 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -246,7 +280,7 @@ CREATE TABLE `test_result` (
 
 LOCK TABLES `test_result` WRITE;
 /*!40000 ALTER TABLE `test_result` DISABLE KEYS */;
-INSERT INTO `test_result` VALUES (123432,'Negative','Negative','Negative','Negative','Negative','2020-03-06'),(124562,'Negative','Negative','Positive','Negative','Positive','2022-01-05'),(934433,'Positive','Negative','Positive','Positive','Positive','2021-01-06');
+INSERT INTO `test_result` VALUES (123432,'Negative','Negative','Negative','Negative','Negative','2020-03-06','Received','NatZel'),(124562,'Negative','Negative','Positive','Negative','Positive','2022-01-05','Received','LilAle'),(696969,'Negative','Negative','Negative','Negative','Positive','2020-03-06','Received','NatZel'),(934433,'Positive','Negative','Positive','Positive','Positive','2021-01-06','Not Received',NULL);
 /*!40000 ALTER TABLE `test_result` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -259,4 +293,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-01-22 16:32:15
+-- Dump completed on 2022-01-25 13:40:32
