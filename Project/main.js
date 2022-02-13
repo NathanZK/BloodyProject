465fbb0c2eb620cdfb3df8309a9c7d171a712744
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2022-02-13 20:19:25 +0300

    Add files via upload

--- Project/main.js
+++ Project/main.js
@@ -16,13 +16,14 @@ const authRoutes = require('./routes/authRoutes');
 
 const admin = require('./routes/admin');
 const donor = require('./routes/donor');
+const guest = require('./routes/guest');
 
 //Routes
 app.use(authRoutes);
 
 app.use(admin);
 app.use(donor);
-app.use(donor);
+app.use(guest);
 
 
 app.listen(port , ()=>console.log("Server running on port ", port));
